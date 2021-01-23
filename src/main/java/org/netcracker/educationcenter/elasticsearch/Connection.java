6c093ab6a66d85da8002429c4adc6154d137f70b
savin000 <58307835+savin000@users.noreply.github.com>
2021-01-24 01:25:41 +0300

    Added Connection.java class to establish connection with Elasticsearch.

--- src/main/java/org/netcracker/educationcenter/elasticsearch/Connection.java
+++ src/main/java/org/netcracker/educationcenter/elasticsearch/Connection.java
@@ -6,25 +6,43 @@ import org.elasticsearch.client.RestHighLevelClient;
 
 import java.io.IOException;
 
+/**
+ * This class establishes a connection with Elasticsearch Database using rest client
+ * to perform requests based on the provided builder.
+ *
+ * @author Mikhail Savin
+ */
 public class Connection {
+    /**
+     * High-level client instance to establish connection
+     */
+    private RestHighLevelClient restHighLevelClient;
 
-    private static final String HOST = "localhost";
-    private static final int PORT_ONE = 9200;
-    private static final int PORT_TWO = 9201;
-    private static final String SCHEME = "http";
-
-    private static RestHighLevelClient restHighLevelClient;
-
-    public synchronized RestHighLevelClient makeConnection() {
-        if (restHighLevelClient == null) {
-            restHighLevelClient = new RestHighLevelClient(
-                    RestClient.builder(
-                            new HttpHost(HOST, PORT_ONE, SCHEME),
-                            new HttpHost(HOST, PORT_TWO, SCHEME)));
-        }
+    /**
+     * @return High-level client instance
+     */
+    public RestHighLevelClient getRestHighLevelClient() {
         return restHighLevelClient;
     }
 
+    /**
+     * High-level client internally creates the low-level client
+     * (which maintains a pool of connections and starts some threads) to perform requests.
+     * Note that we need both ports to be added via builder.
+     */
+    public synchronized void makeConnection() {
+        if (restHighLevelClient == null) {
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(
+                            new HttpHost("142.93.122.167", 9200, "http"),
+                            new HttpHost("142.93.122.167", 9201, "http")));
+        }
+    }
+
+    /**
+     * Closes the client to free resources.
+     * @throws IOException if something wrong with client instance(for example: it doesn't exist)
+     */
     public synchronized void closeConnection() throws IOException {
         restHighLevelClient.close();
         restHighLevelClient = null;
