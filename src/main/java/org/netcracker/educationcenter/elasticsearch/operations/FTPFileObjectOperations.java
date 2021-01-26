48badac830d034f84bd261aad62e50220c2cd87d
savin000 <58307835+savin000@users.noreply.github.com>
2021-01-26 16:30:36 +0300

    Refactored using new ESOperations interface

--- src/main/java/org/netcracker/educationcenter/elasticsearch/operations/FTPFileObjectOperations.java
+++ src/main/java/org/netcracker/educationcenter/elasticsearch/operations/FTPFileObjectOperations.java
@@ -1,31 +1,21 @@
 package org.netcracker.educationcenter.elasticsearch.operations;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.ElasticsearchException;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
-import org.elasticsearch.client.RequestOptions;
 import org.netcracker.educationcenter.elasticsearch.Connection;
 import org.netcracker.educationcenter.elasticsearch.model.FTPFileObject;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * This class implements Elasticsearch Database operations with (on) FTP file objects
  *
  * @author Mikhail Savin
+ * @see ElasticsearchOperations
  */
-public class FTPFileObjectOperations {
+public class FTPFileObjectOperations implements ElasticsearchOperations{
     private static final Logger LOG = LogManager.getLogger();
 
     /**
@@ -52,21 +42,10 @@ public class FTPFileObjectOperations {
      *
      * @param ftpFileObject FTP file object to insert
      */
-    public void insertFTPFileObject(FTPFileObject ftpFileObject) {
-        Map<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("ftpFileObjectId", ftpFileObject.getId());
-        jsonMap.put("ftpFileObjectServer", ftpFileObject.getServer());
-        jsonMap.put("ftpFileObjectText", ftpFileObject.getText());
-        jsonMap.put("ftpFileObjectModificationDate", ftpFileObject.getModificationDate());
-
-        IndexRequest indexRequest = new IndexRequest(INDEX)
-                .id(ftpFileObject.getId()).source(jsonMap);
-        try {
-            IndexResponse indexResponse = connection.getRestHighLevelClient()
-                    .index(indexRequest, RequestOptions.DEFAULT);
-        } catch (IOException e) {
-            LOG.error(e);
-        }
+    public void insertFTPFileObject(FTPFileObject ftpFileObject) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(ftpFileObject);
+        insert(jsonString, ftpFileObject.getId(), INDEX, connection);
     }
 
     /**
@@ -77,20 +56,7 @@ public class FTPFileObjectOperations {
      * @throws IOException if something wrong with get method
      */
     public String getFTPFileObjectById(String id) throws IOException {
-        GetRequest getRequest = new GetRequest(INDEX, id);
-        GetResponse getResponse;
-        try {
-            getResponse = connection.getRestHighLevelClient()
-                    .get(getRequest, RequestOptions.DEFAULT);
-        } catch (ElasticsearchException | IOException e) {
-            LOG.error(e);
-            throw e;
-        }
-        if (getResponse.isExists()) {
-            return getResponse.getSourceAsString();
-        } else {
-            return "Document was not found";
-        }
+        return getById(id, INDEX, connection);
     }
 
     /**
@@ -99,37 +65,18 @@ public class FTPFileObjectOperations {
      * @param id actual FTP file object id
      */
     public void deleteFTPFileObjectById(String id) {
-        DeleteRequest deleteRequest = new DeleteRequest(INDEX, id);
-        try {
-            DeleteResponse deleteResponse = connection.getRestHighLevelClient()
-                    .delete(deleteRequest, RequestOptions.DEFAULT);
-        } catch (java.io.IOException e){
-            LOG.error(e);
-        }
+        deleteById(id, INDEX, connection);
     }
 
     /**
-     * Updated FTP file object by its id with a given reason
+     * Updates FTP file object by its id
      *
      * @param id actual FTP file object id
      * @param ftpFileObject ftpFileObject instance with a new data
-     * @param reason reason for update
      */
-    public void updateFTPFileObjectById(String id, FTPFileObject ftpFileObject, String reason) {
-        Map<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("ftpFileObjectId", ftpFileObject.getId());
-        jsonMap.put("ftpFileObjectServer", ftpFileObject.getServer());
-        jsonMap.put("ftpFileObjectText", ftpFileObject.getText());
-        jsonMap.put("ftpFileObjectModificationDate", ftpFileObject.getModificationDate());
-        jsonMap.put("updated", new Date());
-        jsonMap.put("reason", reason);
-        UpdateRequest updateRequest = new UpdateRequest(INDEX, id)
-                .doc(jsonMap).fetchSource(true); // is fetchSource(true) really needed?
-        try {
-            UpdateResponse updateResponse = connection.getRestHighLevelClient()
-                    .update(updateRequest, RequestOptions.DEFAULT);
-        } catch (java.io.IOException e){
-            LOG.error(e);
-        }
+    public void updateFTPFileObjectById(String id, FTPFileObject ftpFileObject) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(ftpFileObject);
+        updateById(jsonString, id, INDEX, connection);
     }
 }
--- src/main/java/org/netcracker/educationcenter/elasticsearch/operations/JiraIssueOperations.java
+++ src/main/java/org/netcracker/educationcenter/elasticsearch/operations/JiraIssueOperations.java
@@ -1,31 +1,21 @@
 package org.netcracker.educationcenter.elasticsearch.operations;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.ElasticsearchException;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
-import org.elasticsearch.client.RequestOptions;
 import org.netcracker.educationcenter.elasticsearch.Connection;
 import org.netcracker.educationcenter.elasticsearch.model.JiraIssue;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * This class implements Elasticsearch Database operations with (on) Jira-issues
  *
  * @author Mikhail Savin
+ * @see ElasticsearchOperations
  */
-public class JiraIssueOperations{
+public class JiraIssueOperations implements ElasticsearchOperations{
     private static final Logger LOG = LogManager.getLogger();
 
     /**
@@ -52,20 +42,10 @@ public class JiraIssueOperations{
      *
      * @param jiraIssue Jira-issue to insert
      */
-    public void insertJiraIssue(JiraIssue jiraIssue) {
-        Map<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("jiraIssueId", jiraIssue.getId());
-        jsonMap.put("jiraIssueTitle", jiraIssue.getIssueTitle());
-        jsonMap.put("jiraIssueBody", jiraIssue.getIssueBody());
-
-        IndexRequest indexRequest = new IndexRequest(INDEX)
-                .id(jiraIssue.getId()).source(jsonMap);
-        try {
-            IndexResponse indexResponse = connection.getRestHighLevelClient()
-                    .index(indexRequest, RequestOptions.DEFAULT);
-        } catch (IOException e) {
-            LOG.error(e);
-        }
+    public void insertJiraIssue(JiraIssue jiraIssue) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(jiraIssue);
+        insert(jsonString, jiraIssue.getId(), INDEX, connection);
     }
 
     /**
@@ -76,20 +56,7 @@ public class JiraIssueOperations{
      * @throws IOException if something wrong with get method
      */
     public String getJiraIssueById(String id) throws IOException {
-        GetRequest getJiraIssuesRequest = new GetRequest(INDEX, id);
-        GetResponse getResponse;
-        try {
-            getResponse = connection.getRestHighLevelClient()
-                    .get(getJiraIssuesRequest, RequestOptions.DEFAULT);
-        } catch (ElasticsearchException | IOException e) {
-            LOG.error(e);
-            throw e;
-        }
-        if (getResponse.isExists()) {
-            return getResponse.getSourceAsString();
-        } else {
-            return "Document was not found";
-        }
+        return getById(id, INDEX, connection);
     }
 
     /**
@@ -98,36 +65,18 @@ public class JiraIssueOperations{
      * @param id actual Jira-issue id
      */
     public void deleteJiraIssueById(String id) {
-        DeleteRequest deleteRequest = new DeleteRequest(INDEX, id);
-        try {
-            DeleteResponse deleteResponse = connection.getRestHighLevelClient()
-                    .delete(deleteRequest, RequestOptions.DEFAULT);
-        } catch (java.io.IOException e){
-            LOG.error(e);
-        }
+        deleteById(id, INDEX, connection);
     }
 
     /**
-     * Updated Jira-issue by its id with a given reason
+     * Updates Jira-issue by its id
      *
      * @param id actual Jira-issue id
      * @param jiraIssue jiraIssue instance with a new data
-     * @param reason reason for update
      */
-    public void updateJiraIssueById(String id, JiraIssue jiraIssue, String reason) {
-        Map<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("jiraIssueId", jiraIssue.getId());
-        jsonMap.put("jiraIssueTitle", jiraIssue.getIssueTitle());
-        jsonMap.put("jiraIssueBody", jiraIssue.getIssueBody());
-        jsonMap.put("updated", new Date());
-        jsonMap.put("reason", reason);
-        UpdateRequest updateRequest = new UpdateRequest(INDEX, id)
-                .doc(jsonMap).fetchSource(true); // is fetchSource(true) really needed?
-        try {
-            UpdateResponse updateResponse = connection.getRestHighLevelClient()
-                    .update(updateRequest, RequestOptions.DEFAULT);
-        } catch (java.io.IOException e){
-            LOG.error(e);
-        }
+    public void updateJiraIssueById(String id, JiraIssue jiraIssue) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(jiraIssue);
+        updateById(jsonString, id, INDEX, connection);
     }
 }
