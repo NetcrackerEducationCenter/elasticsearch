fbd242de158e23477f40725186074d84b0ef66d2
Mikhail Savin <58307835+savin000@users.noreply.github.com>
2021-02-04 20:09:56 +0300

    Added class for database constants. Now it keeps only index constants

---
+++ src/main/java/org/netcracker/educationcenter/elasticsearch/database/DatabaseConstants.java
@@ -0,0 +1,19 @@
+package org.netcracker.educationcenter.elasticsearch.database;
+
+/**
+ * This class stores constants for classes in the database package
+ *
+ * @author Mikhail Savin
+ */
+public final class DatabaseConstants {
+
+    /**
+     * Jira-issue model's index
+     */
+    public static final String JIRA_ISSUES_INDEX = "jiraissues";
+
+    /**
+     * FTP file object model's index
+     */
+    private static final String FTP_FILE_OBJECTS_INDEX = "ftpfileobjects";
+}
