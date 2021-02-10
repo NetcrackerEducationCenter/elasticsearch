f90004bfe600ef312433980752af98ab2444d4d9
Mikhail Savin <58307835+savin000@users.noreply.github.com>
2021-02-11 00:10:46 +0300

    Property names moved to ConnectionConstants class as constants

---
+++ src/main/java/org/netcracker/educationcenter/elasticsearch/connection/ConnectionConstants.java
@@ -0,0 +1,29 @@
+package org.netcracker.educationcenter.elasticsearch.connection;
+
+/**
+ * This class stores constants for classes in the connection package
+ *
+ * @author Mikhail Savin
+ */
+public final class ConnectionConstants {
+
+    /**
+     * Elasticsearch hostname property name
+     */
+    public static final String HOSTNAME_PROPERTY_NAME = "hostname";
+
+    /**
+     * Scheme property name
+     */
+    public static final String SCHEME_PROPERTY_NAME = "scheme";
+
+    /**
+     * First port property name.
+     */
+    public static final String PORT1_PROPERTY_NAME = "port1";
+
+    /**
+     * Second port property name.
+     */
+    public static final String PORT2_PROPERTY_NAME = "port2";
+}
