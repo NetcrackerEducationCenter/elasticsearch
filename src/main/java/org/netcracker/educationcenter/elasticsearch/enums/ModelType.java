64df188a3c9b85f166641608d4ccc0b9dc6d5adb
Mikhail Savin <58307835+savin000@users.noreply.github.com>
2021-02-14 20:30:31 +0300

    Model type enum added

---
+++ src/main/java/org/netcracker/educationcenter/elasticsearch/enums/ModelType.java
@@ -0,0 +1,19 @@
+package org.netcracker.educationcenter.elasticsearch.enums;
+
+/**
+ * Model types. Used in analysis
+ *
+ * @author Mikhail Savin
+ */
+public enum ModelType {
+
+    /**
+     * Jira issue model type
+     */
+    JIRA_ISSUE,
+
+    /**
+     * FTP file model type
+     */
+    FTP_FILE
+}
